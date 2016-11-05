d050328ea5347156497bcca5d662385d246ec7c3
josh.utterback@gmail.com <josh.utterback@gmail.com>
2016-11-05 16:53:09 -0400

    Clean up old shtml files

--- football/base/menu.php
+++ football/base/menu.php
@@ -163,14 +163,14 @@ A.sideButton:hover {color:#e2a500; text-decoration: none; font-weight: bold}
 <IMG SRC="/images/blank.gif" HEIGHT=11><BR>
       <IMG SRC="/images/logo3.png" ALT="WMFFL" width="145"><BR>
 <IMG SRC="/images/blank.gif" HEIGHT=20><BR>
-    <div class="sideButton"><a class="sideButton" href="/index.php">Front Page</a></div>
+    <div class="sideButton"><a class="sideButton" href="/">Front Page</a></div>
     <div class="sideButton"><a class="sideButton" href="/activate/activations.php">Activations</a></div>
-    <div class="sideButton"><a class="sideButton" href="/teams/">Teams</a></div>
+    <div class="sideButton"><a class="sideButton" href="/teams">Teams</a></div>
     <div class="sideButton"><a class="sideButton" href="/history/2016Season/schedule.php">Schedule</a></div>
     <div class="sideButton"><a class="sideButton" href="/history/2016Season/standings.php">Standings</a></div>
     <div class="sideButton"><a class="sideButton" href="/transactions/transactions.php">Transactions</a></div>
     <div class="sideButton"><a class="sideButton" href="/rules/index.php">Rules</a></div>
-    <div class="sideButton"><a class="sideButton" href="/history.php">History</a></div>
+    <div class="sideButton"><a class="sideButton" href="/history">History</a></div>
 
 <? include "login/logininc.php"; ?>
 
