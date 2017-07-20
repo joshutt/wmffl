cbe84c01eb4292dc3ead617e35925add6d2895f8
josh.utterback@gmail.com <josh.utterback@gmail.com>
2017-07-20 13:31:54 -0400

    Clean up of proposals formatting

--- football/quicklinks.php
+++ football/quicklinks.php
@@ -2,25 +2,25 @@
 <tr><td class="cat" colspan="2">Other Links</td></tr>
 
 <tr><td align="left" width="*">
-<p><a href="/transactions/showprotections.php">Show Protections</a></p>
+<p><a href="/history/2017Season/draftorder.php">2017 Draft Order</a></p>
 </td>
 
 <td align="right" width="*">
+<p><a href="/history/2017Season/protectioncost.php">2017 Protection Costs</a></p>
+</td>
+</tr>
+
+<tr><td align="left" width="*">
 <p><a href="/history/2016Season/teammoney.php">Updated Accounting</a></p>
 </td>
-</tr>
-
-<tr><td align="left" width="*">
-<p><a href="/history/2016Season/draftorder.php">2016 Draft Order</a></p>
-</td>
 
 <td align="right" width="*">
-<p><a href="/history/2016Season/protectioncost.php">2016 Protection Costs</a></p>
+<p><a href="/history/2016Season/draftresults">2016 Draft Results</a></p>
 </td>
 </tr>
 
 <tr><td align="left" width="*">
-<p><a href="/rules/proposals2016.php">Rule Proposals</a></p>
+<p><a href="/rules/proposals2017.php">Rule Proposals</a></p>
 </td>
 
 <td align="right" width="*">
--- football/rules/proposals2017.php
+++ football/rules/proposals2017.php
@@ -2,20 +2,14 @@
 session_start();
 $title = "WMFFL Rule Proposals";
 
+$cssList = array("rules.css");
+include "base/menu.php"; 
 ?>
-
-<style>
-.ballot:link {color: #ff0000; text-decoration: none;}
-.ballot:visited {color: #ff0000; text-decoration: none;}
-.ballot:hover {color: #e2a500; text-decoration: none;}
-</style>
-
-<? include "base/menu.php"; ?>
 
 <h1 align=center>Current Rule Proposals</h1>
 <hr SIZE = "1"/>
 
-<p>This is the list of proposals to be voted on for the 2016 WMFFL season.
+<p>This is the list of proposals to be voted on for the 2017 WMFFL season.
 If you would like to suggest a rule proposal, you may
 do so on the <a href="proposesubmit.php">proposals page</a>.  The part of
 each proposal that appears in <i>Italics</i> is what effect this proposal will
@@ -24,7 +18,7 @@ have on the ruleset, if passed.</p>
 <p>
 <b>Proposal 2015.2 - League Champion Renames Toilet Bowl Loser</b><br/>
 <b>Sponsor: Tom Marsh and Josh Utterback</b><br/>
-<a class="ballot">Status: <font color="red">On Ballot</font></a><br/>
+<span class="ballot">Status: <span class="status">On Ballot</span></span><br/>
 At the end of the season the League Champion can rename the Toilet Bowl loser's team.  They have to keep this name until they either make the playoffs, lose the Toilet Bowl again or change owners.
 <blockquote><i>Update rule III.E to read:
     <blockquote>E. The two last place teams in each division with the worse records will play in week 15 for the Toilet Bowl<br/>
@@ -41,7 +35,7 @@ At the end of the season the League Champion can rename the Toilet Bowl loser's 
 <p>
 <b>Proposal 2016.4 - Pay by the Draft or Don't Play</b><br/>
 <b>Sponsor: Tim Shoobridge</b></br/>
-<a class="ballot">Status: <font color="red">On Ballot</font></a><br/>
+<span class="ballot">Status: <span class="status">On Ballot</span></span><br/>
 Any owner that hasn't paid by draft day will not be allowed to play.
 <blockquote><i>Update rule IX.A.1 to read:
     <blockquote>1. Failure to pay the entry fee by the date of the draft will make an owner inelgible to play that season.<br/>
@@ -52,7 +46,7 @@ Any owner that hasn't paid by draft day will not be allowed to play.
 <p>
 <b>Proposal 2017.1 - Increase Draft Time</b><br/>
 <b>Sponsor: Josh Utterback</b></br/>
-<a class="ballot">Status: <font color="red">In Discussion</font></a><br/>
+<span class="ballot">Status: <span class="status">In Discussion</span></span><br/>
 Increase the time each pick adds to the draft clock to 45 seconds.
 <blockquote><i>In rule VII.B.2 change <b>30 seconds</b> to <b>45 seconds</b></i></blockquote>
 </p>
@@ -60,7 +54,7 @@ Increase the time each pick adds to the draft clock to 45 seconds.
 <p>
 <b>Proposal 2017.2 - Unpaid Teams Can't Purchase Transactions</b><br/>
 <b>Sponsor: Josh Utterback</b></br/>
-<a class="ballot">Status: <font color="red">In Discussion</font></a><br/>
+<span class="ballot">Status: <span class="status">In Discussion</span></span><br/>
 Make it so that teams that have not yet paid their entry fee are not allowed to purchase extra transactions.
 <blockquote><i>Add rule IX.C.4 to read:
     <blockquote>4. No team that has failed to pay their entry fee may purchase extra transactions</blockquote>
@@ -70,7 +64,7 @@ Make it so that teams that have not yet paid their entry fee are not allowed to 
 <p>
 <b>Proposal 2017.3 - Add Game Planning</b><br/>
 <b>Sponsor: Josh Utterback</b></br/>
-<a class="ballot">Status: <font color="red">In Discussion</font></a><br/>
+<span class="ballot">Status: <span class="status">In Discussion</span></span><br/>
 Make it so that teams can game plan for their opponents.  A team can name one player on their roster and one on their opponent, that they game plan for.
 The player on their team that they have game planned around will earn double points.  The player on the other team that they have game planned around will
 only earn hald of their points.  If both teams game plan for the same player then they cancel each other other.  All game planning must be done before
