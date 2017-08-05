b3bfd9c1414a43f683078698b9ac4676d4efe251
josh.utterback@gmail.com <josh.utterback@gmail.com>
2017-08-04 23:27:52 -0400

    Initial Cleanup, no changes

---
+++ football/base/js/index.js
@@ -0,0 +1,13 @@
+
+	function changenews ()
+	{
+		var uid = document.getElementById("news").value;
+		location.href = 'index.php?uid=' + uid;
+	}
+
+
+	function changeyear ()
+	{
+		var uid = document.getElementById("artSeason").value;
+		location.href = 'index.php?artSeason=' + uid;
+	}
--- football/base/test.php
+++
@@ -1,7 +0,0 @@
-<?
-print "First Doc Root<br/>";
-print $DOCUMENT_ROOT;
-print "<br/>Next Doc Root<br/>";
-print $_SERVER['DOCUMENT_ROOT'];
-
-?>
