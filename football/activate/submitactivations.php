465053e269f0bf5588f34348fc7091fbd660cf0c
Josh <josh.utterback@gmail.com>
2017-09-02 17:51:09 -0400

    Move javascript to separate file

--- football/activate/submitactivations.php
+++ football/activate/submitactivations.php
@@ -1,4 +1,6 @@
-<? 
+<?
+$javascriptList = array("/base/js/activations.js");
+
 require_once "$DOCUMENT_ROOT/utils/start.php";
 require_once "$DOCUMENT_ROOT/base/conn.php";
 require_once "$DOCUMENT_ROOT/login/loginglob.php";
@@ -204,68 +206,6 @@ if ($actingHC) {
 
 
 ?>
-
-<script language="javascript">
-<!-- 
-function swapActivations(theForm) {
-    var week = theForm.value;
-    makeHttpRequest('weekSubAct.php?week='+week, 'changeTable', 0);
-
-}
-
-function changeTable(responseText) {
-    document.getElementById("subAct").innerHTML = responseText;
-}
-
-function makeHttpRequest(url, callback_function, returnXML, post) {
-
-    var httpRequest = false;
-
-    if (window.XMLHttpRequest) {
-        httpRequest = new XMLHttpRequest();
-        if (httpRequest.overrideMimeType) {
-            httpRequest.overrideMimeType('text/xml');
-        }
-    } else if (window.ActiveXObject) {
-        try {
-            httpRequest = new ActiveXObject("Msxml2.XMLHTTP");
-        } catch (e) {
-            try {
-                httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
-            } catch (e) {}
-        }
-    }
-
-    if (!httpRequest) {
-        alert ('Unfortunately your browser doesn\'t support this feature.');
-        return false;
-    }
-    if (callback_function) {
-        httpRequest.onreadystatechange = function() {
-            if (httpRequest.readyState == 4) {
-                if (httpRequest.status == 200) {
-                    if (returnXML) {
-                        eval(callback_function + '(httpRequest.responseXML)');
-                    } else {
-                        eval(callback_function + '(httpRequest.responseText)');
-                    }
-                }
-            }
-        }
-    }
-
-    if (post) {
-        httpRequest.open('POST', url, true);
-        httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-        httpRequest.send(post);
-
-    } else {
-        httpRequest.open('GET', url, true);
-        httpRequest.send(null);
-    }
-}
--->
-</script>
 
 <H1 ALIGN=Center>Activations</H1>
 <HR size = "1">
