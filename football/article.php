c86397e999caf72b7cb88261fa82a10ef58d3152
josh.utterback@gmail.com <3430+joshutt@users.noreply.github.com>
2018-10-26 08:58:12 -0400

    Update article seasons

--- football/article.php
+++ football/article.php
@@ -76,7 +76,7 @@ $articles->find();
 			</select>
             <select id="artSeason" style="margin:5px" onchange="changeyear()">
 <?
-$years = array(2016, 2015, 2014, 2013, 2012, 2011, 2010, 2009, 2008, 2007, 2006);
+$years = array(2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011, 2010, 2009, 2008, 2007, 2006);
 
 foreach($years as $y) {
     $st = "";
