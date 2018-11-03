0b4fc8f2e675a40a240bcaf2fca29c063bcabb26
Josh <3430+joshutt@users.noreply.github.com>
2018-11-02 20:15:59 -0400

    Fix front page after bootstrap import

--- football/forum/commentlist.php
+++ football/forum/commentlist.php
@@ -12,39 +12,20 @@ $post->find();
 ?>
 
 <style>
-  .NFLHeaderText {color:Red; text-decoration:bold; font-size:14pt}
-  //.Headline  {color:Brown} 
-  A.NFLHeadline:link {font-size:10pt; text-decoration:none; color:Brown}
-  A.NFLHeadline:visited {font-size:10pt; text-decoration:none; color:Brown}
-  A.NFLHeadline:hover {font-size:10pt; color:Red}
-  .NFLNewsText  {font-size:10pt; color:Orange}
-  A.NFLNewsText:link {text-decoration:none}
-  A.NFLNewsText:visited {text-decoration:none}
-  .NFLNewsDate {font-size:8pt}
-  A.Comment:link {font-size:10pt; text-decoration:none; color:e2a500}
-  A.Comment:visited {font-size:10pt; text-decoration:none; color:e2a500}
-  A.Comment:hover {font-size:10pt; color:Brown}
 </STYLE>
 
-<TABLE ALIGN="left" BORDER='0' WIDTH='244' CELLPADDING='1' CELLSPACING='0'>
-<TR><TD><TABLE BGCOLOR='#eeeeee' CELLPADDING='6' CELLSPACING='0'  border='0' WIDTH='244'>
-<TR><TD height='24' align='center' bgcolor="#660000">
-<FONT class='SectionHeader'>LATEST TRASH TALK</FONT></TD></TR>
 
-
-
-<?
+<div class="cat text-center">LATEST TRASH TALK</div>
+<div class="my-1 text-left gameBox">
+    <?php
 while ($post->fetch()) {
-    print "<TR><TD><A HREF=\"/forum/comments.php\" class=\"NFLHeadline\">";
-	print "<FONT class=\"NFLHeadline\">".$post->gettitle()."</FONT></A></TD></TR>";
-	//print "<FONT class=\"NFLHeadline\">".$post->getPrintTitle()."</FONT></A></TD></TR>";
+    ?>
+    <div class="boxScore p-1"><a class="NFLHeadline" href="/forum/comments.php"><?= $post->gettitle() ?></a></div>
+    <?php
 }
-
 ?>
+    <div class="boxScore p-1"><a href="/forum/blogentry.php" class="comment">Leave Commentary</a></div>
+</div>
 
-<tr><td><a href="/forum/blogentry.php" class="Comment">
-Leave Commentary
-</a></td></tr>
 
-</TABLE>
-</table>
+
--- football/standings.php
+++ football/standings.php
@@ -1,76 +1,8 @@
 <?
-require_once "utils/start.php";
+require_once "utils/connect.php";
 
 //include "lib/Team.php";
 
-function printStandings($divName, $divID, $teamArray, $startPt) {
-
-    $tagArray = array ("Bitch Better Have My Money"=>"z-", "Lindbergh Baby Casserole"=>"",
-        "Werewolves"=>"x-", "Sacks on the Beach"=>"x-", "Norsemen"=>"",
-        "Crusaders"=>"", "Rednecks"=>"", "MeggaMen"=>"",
-        "Whiskey Tango"=>"", "Gallic Warriors"=>"");
-
-    print "<TABLE><TH>$divName Division</TH>";
-    print "<TR><TD WIDTH=33%>Team</TD><TD WIDTH=11%>Wins</TD><TD WIDTH=11%>Losses</TD><TD WIDTH=11% ALIGN=Center>Ties</TD><TD WIDTH=15%>PCT</TD><TD WIDTH=10%>PF</TD><TD WIDTH=*>PA</TD></TR>";
-    for ($i = $startPt; $teamArray[$i]->division == $divID; $i++) {
-        print "<TR><TD>";
-        print $tagArray[$teamArray[$i]->name];
-        print "<A HREF=\"/teams/teamroster.php?viewteam=".$teamArray[$i]->teamid."\">";
-        //print "<A HREF=\"/teams/".str_replace(" ","",strtolower($teamArray[$i]->name)).".shtml\">";
-        print $teamArray[$i]->name."</A></TD>";
-        print "<TD ALIGN=CENTER>".$teamArray[$i]->record[0]."</TD>";
-        print "<TD ALIGN=CENTER>".$teamArray[$i]->record[1]."</TD>";
-        print "<TD ALIGN=CENTER>".$teamArray[$i]->record[2]."</TD>";
-        printf ("<TD>%5.3f</TD>", $teamArray[$i]->getWinPCT());
-        print "<TD>".$teamArray[$i]->ptsFor."</TD>";
-        print "<TD>".$teamArray[$i]->ptsAgt."</TD></TR>";
-    }
-
-    print "</TABLE><BR>";
-
-}
-
-
-function nothing()
-{
-
-    $sql = "select tn1.name as name, t1.divisionid as division, ";
-    $sql .= "if (t1.teamid=s.teama, s.scorea, s.scoreb) as ptsfor, ";
-    $sql .= "if (t1.teamid=s.teamb, s.scorea, s.scoreb) as ptsag, ";
-    $sql .= "tn2.name as opp, t2.divisionid as oppdiv, s.week  as week, ";
-    $sql .= "t1.teamid as teamid ";
-    $sql .= "from schedule s, team t1, team t2, teamnames tn1, teamnames tn2 ";
-    $sql .= "where s.season=$currentSeason and s.week<$currentWeek ";
-    $sql .= "and s.week<=14 ";
-    $sql .= "and t1.teamid in (s.teama, s.teamb) ";
-    $sql .= "and t2.teamid in (s.teama, s.teamb) ";
-    $sql .= "and t1.teamid<>t2.teamid ";
-    $sql .= "and t1.teamid=tn1.teamid and t2.teamid=tn2.teamid ";
-    $sql .= "and tn1.season=s.season and tn2.season=s.season ";
-
-    $teamArray = array();
-
-    $results = mysql_query($sql) or die(mysql_error());
-    while ($games = mysql_fetch_array($results)) {
-        $teamName = $games["name"];
-        if (!array_key_exists($teamName, $teamArray)) {
-            $teamArray[$teamName] = new Team($teamName, $games["division"], $games["teamid"]);
-        }
-        $teamArray[$teamName]->addGame($games["opp"], $games["ptsfor"], $games["ptsag"], $games["oppdiv"]);
-    }
-
-    usort($teamArray, "orderteam");
-
-
-    printStandings("Burgandy", 1, $teamArray, 0);
-    printStandings("Gold", 2, $teamArray, 5);
-}
-/*
-foreach ($teamArray as $team) {
-    print $team->name." - ".$team->ptsFor." ".$team->ptsAgt." ";
-    printf ("%5.3f - %5.3f<BR>", $team->getWinPCT(), $team->getDivWinPCT());
-}
-*/
 
 $thisSeason = $currentSeason;
 $thisWeek = $currentWeek;
@@ -82,24 +14,24 @@ $display = 0;
 include "history/common/weekstandings.php";
 ?>
 
-<TABLE ALIGN="left" BORDER='0' WIDTH='244' CELLPADDING='1' CELLSPACING='0'>
-<TR><TD><TABLE BGCOLOR='#eeeeee' CELLPADDING='0' CELLSPACING='0'  border='0' WIDTH='244'>
-<TR><TD height='32' align='center' bgcolor="#660000" colspan="2">
-<FONT class='SectionHeader'>STANDINGS</FONT></TD></TR>
-
+<div class="cat text-center">STANDINGS</div>
+<div class="container gameBox pb-1">
 <?php
 $thisDiv = "";
 foreach ($teamArray as $t) {
+    $margin = "mt-0";
     if ($t->division != $thisDiv) {
         $thisDiv = $t->division;
+        $margin = "mt-2";
         ?>
-            <tr height="10"><td height="10"></td></tr>
-<? } ?>
-    <tr height="10">
-        <td height="10"><a href="" class="NFLHeadline"><?= $t->name ?></a></td>
-                <td><a href="" class="NFLHeadline"><?= $t->getPrintRecord() ?></a></td>
-    </tr>
-<? } ?>
+    <?php } ?>
+    <div class="row <?= $margin ?>">
+        <div class="boxScore col-6 pl-1 text-left"><?= $t->name ?></div>
+        <div class="boxScore col-6  text-right"><?= $t->getPrintRecord() ?></div>
+    </div>
+<?php } ?>
+</div>
 
-<tr height="10"><td height="10"></td></tr>
-</TABLE></TD></TR></TABLE>
+
+
+
