86f62090bcddcba0fd089f494b5ba6161e4c02cd
josh.utterback@gmail.com <3430+joshutt@users.noreply.github.com>
2018-12-28 16:23:25 -0500

    Added new rule proposals

--- football/quicklinks.php
+++ football/quicklinks.php
@@ -1,9 +1,9 @@
 <div class="cat">Other Links</div>
 
 <ul class="list-group">
+    <li class="list-group-item float-left col-lg-4 col-md-6 col-12"><a href="/rules/proposals2019">Rule Proposals</a> </li>
     <li class="list-group-item float-left col-lg-4 col-md-6 col-12"><a href="/history/2019Season/draftorder">2019 Draft Order</a></li>
-    <li class="list-group-item float-left col-lg-4 col-md-6 col-12"><a href="/history/2018Season/draftresults">Draft Results</a></li>
+    <li class="list-group-item float-left col-lg-4 col-md-6 col-12"><a href="/history/2018Season/teammoney">Winnings</a></li>
     <li class="list-group-item float-left col-lg-4 col-md-6 col-12"><a href="/history/2019Season/protectioncost">2019 Protection Costs</a></li>
-    <li class="list-group-item float-left col-lg-4 col-md-6 col-12"><a href="/rules/proposals2018">Rule Proposals</a> </li>
 </ul>
 
---
+++ football/rules/proposals2019.php
@@ -0,0 +1,37 @@
+<?
+session_start();
+$title = "WMFFL Rule Proposals";
+
+$cssList = array("rules.css");
+include "base/menu.php"; 
+?>
+
+<h1 align=center>Current Rule Proposals</h1>
+<hr SIZE = "1"/>
+
+<p>This is the list of proposals to be voted on for the 2018 WMFFL season.
+If you would like to suggest a rule proposal, you may
+do so on the <a href="proposesubmit.php">proposals page</a>.  The part of
+each proposal that appears in <i>Italics</i> is what effect this proposal will
+have on the ruleset, if passed.</p>
+
+<p>
+<b>Proposal 2019.1 - Reduce the length of the draft</b><br/>
+<b>Sponsor: Richard Lawson</b><br/>
+<span class="ballot">Status: <span class="status">In Discussion</span></span><br/>
+Reduce the length of the draft to 14 rounds
+<blockquote><i>Update rules VI.D.1 and .2 to read:
+    <blockquote>1. The first and second quarters will consist of four rounds.<br/>
+    2. The third and fourth quarters will consist of three rounds.</blockquote>
+    </i></blockquote>
+</p>
+
+<p>
+<b>Proposal 2019.2 - Reduce the size of rosters</b><br/>
+<b>Sponsor: Richard Lawson</b><br/>
+<span class="ballot">Status: <span class="status">In Discussion</span></span><br/>
+Reduce the size of rosters to 23 players
+<blockquote><i>In rule IV.A change the number 25 to 23</i></blockquote>
+</p>
+
+<? include "base/footer.html"; ?>
