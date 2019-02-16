c583d5bfbaf090cbaaaef6ba250a54db758b1ddf
josh.utterback@gmail.com <3430+joshutt@users.noreply.github.com>
2019-02-16 12:18:40 -0500

    Clean up setup and .ini files

--- football/utils/setup.php
+++ football/utils/setup.php
@@ -1,21 +1,13 @@
 <?php
-date_default_timezone_set('America/New_York');
+// Paths to config files should be set in .user.ini file
+$ini = parse_ini_file("wmffl.conf");
 
-#$SCRIPT_PATH = "/home/joshutt/git/lib";
-$DIR_ROOT = "C:/Users/Josh/IdeaProjects/wmffl";
-$SCRIPT_PATH = "$DIR_ROOT/lib";
-$CONF_PATH = "$DIR_ROOT/conf";
-
-ini_set("include_path", "/home/joshutt/php:$SCRIPT_PATH:$CONF_PATH" . ini_get("include_path"));
-$ini = parse_ini_file("$CONF_PATH/wmffl.conf");
-#$ini = parse_ini_file("wmffl.conf");
-
-#require_once "PEAR.php";
 require_once "DB/DataObject.php";
 
 $options = &PEAR::getStaticProperty('DB_DataObject', 'options');
 #$config = parse_ini_file("$SCRIPT_PATH/dataobjects.ini", TRUE);
-$config = parse_ini_file("$CONF_PATH/wmffl.conf", TRUE);
+#$config = parse_ini_file("$CONF_PATH/wmffl.conf", TRUE);
+$config = parse_ini_file("wmffl.conf", TRUE);
 $options = $config['DB_DataObject'];
 $debug = 5;
 
