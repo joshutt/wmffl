a35e4aaca08396d2a7e71c3474b07198cb5cf7ff
josh.utterback@gmail.com <3430+joshutt@users.noreply.github.com>
2019-08-31 19:09:17 -0400

    Post draft updates

--- football/quicklinks.php
+++ football/quicklinks.php
@@ -1,6 +1,7 @@
 <div class="cat">Other Links</div>
 
 <ul class="list-group">
+    <li class="list-group-item float-left col-lg-4 col-md-6 col-12"><a href="/transactions/livedraft">Live Draft</a></li>
     <li class="list-group-item float-left col-lg-4 col-md-6 col-12"><a href="/transactions/showprotections">View Protections</a></li>
     <li class="list-group-item float-left col-lg-4 col-md-6 col-12"><a href="/history/2019Season/draftorder">2019 Draft Order</a></li>
     <li class="list-group-item float-left col-lg-4 col-md-6 col-12"><a href="/history/2019Season/teammoney">Winnings</a></li>
