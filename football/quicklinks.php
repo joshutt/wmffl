ca01d707dae6f3aa002620208eca7a4f18ecfbf3
josh.utterback@gmail.com <3430+joshutt@users.noreply.github.com>
2020-08-28 16:22:50 -0400

    Update to quicklinks

--- football/quicklinks.php
+++ football/quicklinks.php
@@ -1,6 +1,7 @@
 <div class="cat">Other Links</div>
 
 <ul class="list-group">
+    <li class="list-group-item float-left col-lg-4 col-md-6 col-12"><a href="/rules/ballot">Vote on Rule Changes</a></li>
     <li class="list-group-item float-left col-lg-4 col-md-6 col-12"><a href="/transactions/showprotections">Protections</a></li>
     <li class="list-group-item float-left col-lg-4 col-md-6 col-12"><a href="/rules/proposals2020">Rule Change Proposals</a></li>
     <li class="list-group-item float-left col-lg-4 col-md-6 col-12"><a href="/history/2020Season/draftorder">Draft Order</a></li>
