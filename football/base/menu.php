f4b513d7df9d77a25543c7da5816f59d229a2045
josh.utterback@gmail.com <3430+joshutt@users.noreply.github.com>
2020-09-22 12:37:19 -0400

    Add 2020 schedule

--- football/base/menu.php
+++ football/base/menu.php
@@ -53,7 +53,7 @@ if (!isset($title)) {
             <div class="sideButton"><a class="sideButton" href="/teams">Teams</a></div>
             <div class="sideButton"><a class="sideButton" href="/stats/leaders">Stats</a></div>
             <div class="sideButton"><a class="sideButton" href="/history/2020Season/schedule">Schedule</a></div>
-            <div class="sideButton"><a class="sideButton" href="/history/2019Season/standings">Standings</a></div>
+            <div class="sideButton"><a class="sideButton" href="/history/2020Season/standings">Standings</a></div>
             <div class="sideButton"><a class="sideButton" href="/transactions/transactions">Transactions</a></div>
             <div class="sideButton"><a class="sideButton" href="/rules">Rules</a></div>
             <div class="sideButton"><a class="sideButton" href="/history">History</a></div>
