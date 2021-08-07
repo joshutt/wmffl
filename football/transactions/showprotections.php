5eb7b16e882d14af23bb1f0460b593ecca423a00
Josh <3430+joshutt@users.noreply.github.com>
2021-08-07 19:53:19 -0400

    Modernize showprotections

--- football/transactions/showprotections.php
+++ football/transactions/showprotections.php
@@ -1,11 +1,7 @@
-<?
-require_once "utils/start.php";
+<?php
+require_once 'utils/start.php';
 
-if (isset($season)) {
-    $lookseason = $season;
-} else {
-    $lookseason = $currentSeason;
-}
+$lookseason = $season ?? $currentSeason;
 
 //$query = "SELECT t.name, CONCAT(p.firstname, ' ',p.lastname), ";
 //$query .= "p.pos, p.team, pro.cost ";
@@ -22,19 +18,19 @@ JOIN teamnames t on t.teamid=pro.teamid and t.season=pro.season
   WHERE pro.season=$lookseason ";
 
 
-if (!isset($order) || $order=='team') {
-    $query .= "ORDER BY t.name, p.pos, p.lastname ";
+if (!isset($order) || $order == 'team') {
+    $query .= 'ORDER BY t.name, p.pos, p.lastname ';
     $teamcheck = true;
 } else {
-    $query .= "ORDER BY p.pos, p.lastname ";
+    $query .= 'ORDER BY p.pos, p.lastname ';
     $teamcheck = false;
 }
 
 $displayArray = array();
-$result = mysqli_query($conn, $query) or die("Error: " . mysqli_error($conn));
+$result = mysqli_query($conn, $query) or die('Error: ' . mysqli_error($conn));
 while (list($team, $name, $pos, $nfl, $cost) = mysqli_fetch_row($result)) {
     if ($teamcheck) {
-        $labels = array("Name", "Position", "NFL Team", "Cost");
+        $labels = array('Name' => 6, 'Pos' => 2, 'NFL' => 2, 'Cost' => 2);
         if ($oldteam != $team) {
             $teamArray = array();
             $displayArray[$team] = array();
@@ -42,7 +38,7 @@ while (list($team, $name, $pos, $nfl, $cost) = mysqli_fetch_row($result)) {
         }
         array_push($displayArray[$team], array($name, $pos, $nfl, $cost));
     } else {
-        $labels = array("Team", "Name", "NFL Team", "Cost");
+        $labels = array('Team' => 4, 'Name' => 4, 'NFL' => 2, 'Cost' => 2);
         if ($oldpos != $pos) {
             $teamArray = array();
             $displayArray[$pos] = array();
@@ -53,47 +49,66 @@ while (list($team, $name, $pos, $nfl, $cost) = mysqli_fetch_row($result)) {
 }
 
 
-$title = "WMFFL Protections";
+$title = 'WMFFL Protections';
 ?>
 
-<?
-        include "base/menu.php"; 
-?>      
+<?php
+include 'base/menu.php';
+?>
 
 <H1 ALIGN=Center>Protections</H1>
-<HR size = "1">
+<HR size="1">
 
-<P ALIGN=Center><A HREF="showprotections.php?order=team">By Team</A>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-<A HREF="showprotections.php?order=pos">By Position</A>
+<P ALIGN=Center><a class="btn btn-wmffl" href="showprotections.php?order=team&season=<?= $lookseason ?>">By Team</a>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <A class="btn btn-wmffl" HREF="showprotections.php?order=pos&season=<?= $lookseason ?>">By Position</a>
 </P>
 
-<TABLE WIDTH=100%>
-<?
-$result = mysqli_query($conn, $query) or die("Error: " . mysqli_error($conn));
+<div class="container-fluid">
+<div class="row">
+<?php
+$result = mysqli_query($conn, $query) or die('Error: ' . mysqli_error($conn));
 
-foreach ($displayArray as $key=>$teamArray) {
-    print "<tr><th colspan=\"4\">$key (".sizeof($teamArray).")</th></tr>";
-    print "<tr>";
-    foreach ($labels as $label) {
-        print "<th align=\"left\">$label</th>";
-    }
-    print "</tr>";
-    foreach ($teamArray as $team) {
-        print "<tr>";
-        foreach ($team as $item) {
-            print "<td>$item</td>";
-        }
-        print "</tr>";
-    }
-    print "<tr><td></td></tr>";
+foreach ($displayArray as $key => $teamArray) {
+    ?>
+        <div class="col-12 col-lg-6 col-xl-4">
+    <div class="card px-0 mx-0 my-2" >
+        <div class="card-header text-center font-weight-bold"><?= $key ?> (<?= sizeof($teamArray) ?>)</div>
+        <div class="card-body">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <?php
+                    foreach ($labels as $label => $weight) {
+                    ?>
+                    <div class="col-<?=$weight?> px-lg-1"><strong><?= $label ?></strong></div>
+                    <?php } ?>
+                </div>
+                <?php
+                foreach ($teamArray as $team) {
+                    ?>
+                    <div class="row justify-content-center">
+                        <?php
+                        $c = 0;
+                        foreach ($team as $item) {
+                            ?>
+                            <div class="col-<?= array_values($labels)[$c] ?> px-lg-0"><?= $item ?></div>
+                        <?php
+                        $c++;
+                        } ?>
+                    </div>
+                <?php } ?>
+            </div>
+        </div>
+    </div>
+        </div>
+
+<?php
 }
 ?>
-</TABLE>
-
-<?
-        include "base/footer.html";
+</div></div>
+<?php
+include 'base/footer.html';
 ?>
 
 
