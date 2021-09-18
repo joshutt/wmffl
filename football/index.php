7ace4ddde2f192087bb9d88ff90c2c8a979d97ab
Josh <3430+joshutt@users.noreply.github.com>
2021-09-18 15:28:14 -0400

    Tweaking front page looks

--- football/index.php
+++ football/index.php
@@ -8,57 +8,51 @@ include 'base/menu.php';
 ?>
 
 <div class="row">
-    <div class="col-8">
+    <div class="col-md-8 col">
 
         <div class="m-2">
-            <?php include 'aarticle.php' ?>
+            <?php include 'article.php' ?>
         </div>
 
-        <div class="card m-1">
-            <?php include 'forum/commentlist.php' ?>
-        </div>
 
     </div>
-    <div class="col-4">
+
+    <div class="w-100 d-none d-block d-md-none"><!-- wrap every 2 on sm--></div>
+
+    <div class="col-md-4 col">
+
+        <div class="card text-center m-1 mb-2">
+            <div class="card-header">
+                <ul class="nav nav-tabs card-header-tabs justify-content-center" role="tablist">
+                    <li role="presentation" class="btn btn-wmffl mr-1">
+                        <a href="#scores" aria-controls="tab-01" role="tab" data-toggle="tab">Scores</a>
+                    </li>
+                    <li role="presentation" class="btn btn-wmffl ml-1">
+                        <a href="#standings" aria-controls="tab-02" role="tab" data-toggle="tab">Standings</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="card-body py-1 px-0">
+                <div class="tab-content">
+                    <div role="tabpanel" class="tab-pane active" id="scores">
+                        <?php include 'scores.php' ?>
+                    </div>
+                    <div role="tabpanel" class="tab-pane" id="standings">
+                        <?php include 'standings.php' ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="card m-1 mb-2">
+            <?php include 'forum/commentlist.php' ?>
+        </div>
         <div class="card m-1">
             <?php include 'quicklinks.php'; ?>
         </div>
-<!--        <div class="card m-1">-->
-<!--            --><?php //include 'scores.php' ?>
-<!--        </div>-->
-<!--        <div class="card m-1">-->
-<!--            --><?php //include 'standings.php' ?>
-<!--        </div>-->
     </div>
-
 </div>
 
-<!--<table width="100%" border="0">-->
-<!--    <tr>-->
-<!--        <td VALIGN="top" width="*">-->
-<!---->
-<!--            <div class="card m-1">-->
-<!--                --><?php //include 'article.php' ?>
-<!--            </div>-->
-<!--            <div class="card m-1">-->
-<!--                --><?php //include 'quicklinks.php'; ?>
-<!--            </div>-->
-<!---->
-<!--        </td>-->
-<!---->
-<!--        <td align="right" valign="top" width="260">-->
-<!--            <div class="card m-1">-->
-<!--                --><?php //include 'scores.php' ?>
-<!--            </div>-->
-<!--            <div class="card m-1">-->
-<!--                --><?php //include 'standings.php' ?>
-<!--            </div>-->
-<!--            <div class="card m-1">-->
-<!--                --><?php //include 'forum/commentlist.php' ?>
-<!--            </div>-->
-<!--        </td>-->
-<!---->
-<!--    </tr>-->
-<!--</table>-->
+</div>
 
 <?php include 'base/footer.php'; ?>
--- football/standings.php
+++ football/standings.php
@@ -1,5 +1,5 @@
 <?php
-require_once "utils/connect.php";
+require_once 'utils/connect.php';
 
 
 $thisSeason = $currentSeason;
@@ -9,23 +9,33 @@ if ($thisWeek == 0) {
     $thisSeason = $thisSeason - 1;
 }
 $display = 0;
-include "history/common/weekstandings.php";
+include 'history/common/weekstandings.php';
 ?>
 
 <div class="cat text-center">STANDINGS</div>
 <div class="container gameBox pb-1">
 <?php
-$thisDiv = "";
+$thisDiv = '';
 foreach ($teamArray as $t) {
-    $margin = "mt-0";
+    $margin = 'mt-0';
     if ($t->division != $thisDiv) {
+        if ($thisDiv !== '') {
+            ?>
+</div>
+            <?php
+            $margin = 'mt-3';
+        }
         $thisDiv = $t->division;
-        $margin = "mt-2";
+        ?>
+        <div class="border-bottom border-top p-1">
+            <?php
         ?>
     <?php } ?>
-    <div class="row <?= $margin ?> justify-content-between">
+    <div class="mt-0 justify-content-between">
+        <a class="row m-0" href="/teams/teamschedule?viewteam=<?= $t->teamid ?>">
         <div class="boxScore col-8 pl-1 pr-0 text-left text-truncate"><?= $t->name ?></div>
         <div class="boxScore col-4 px-0 d-block text-center"><?= $t->printShortRecord() ?></div>
+        </a>
     </div>
 <?php } ?>
 </div>
