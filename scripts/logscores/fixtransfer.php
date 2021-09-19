91d52feba4e217ef986e2c52293d0609be2983e8
Josh <3430+joshutt@users.noreply.github.com>
2021-09-19 18:15:51 -0400

    Corrections to transferscores script

--- scripts/logscores/fixtransfer.php
+++ scripts/logscores/fixtransfer.php
@@ -1,55 +1,56 @@
-<?
-require_once dirname(__FILE__)."/../base.php";
+<?php
+require_once dirname(__FILE__) . '/../base.php';
 
-function catchBadFunction($errno, $errstr, $errfile, $errline, $vars) {
+function catchBadFunction($errno, $errstr, $errfile, $errline, &$vars)
+{
     error_log("$errstr in $errfile on line $errline");
     if ($errno == FATAL || $errno == ERROR) {
         exit(1);
     }
 //    print_r ($vars);
-    print "Problem with player: ".$vars["player"]["playerid"];
-    print " - Pos: ".$vars["player"]["position"]."\n";
-    $vars["pts"] = 0;
+    print 'Problem with player: ' . $vars['player']['playerid'];
+    print ' - Pos: ' . $vars['player']['position'] . "\n";
+    $vars['pts'] = 0;
 }
 
 //require_once "/home/joshutt/football/utils/start.php";
 //include "/home/joshutt/football/base/scoring.php";
-include "base/scoring.php";
+include 'base/scoring.php';
 
 $week = $currentWeek - 1;
 //$week = $currentWeek;
 print $week;
 print "\n";
 
-$sql = "select p.playerid, p.pos, s.season, s.* ";
-$sql .= "from newplayers p ";
-$sql .= "join stats s on s.statid=p.flmid ";
-$sql .= "left join playerscores ps on ps.playerid=p.playerid and ps.season=s.season and ps.week=s.week ";
+$sql = 'select p.playerid, p.pos, s.season, s.* ';
+$sql .= 'from newplayers p ';
+$sql .= 'join stats s on s.statid=p.flmid ';
+$sql .= 'left join playerscores ps on ps.playerid=p.playerid and ps.season=s.season and ps.week=s.week ';
 $sql .= "where s.season=$currentSeason and s.week=$week";
-$sql .= " and ps.playerid is null ";
+$sql .= ' and ps.playerid is null ';
 
-$bigquery = "insert into playerscores (playerid, season, week, pts) ";
-$bigquery .= "values ";
+$bigquery = 'insert into playerscores (playerid, season, week, pts) ';
+$bigquery .= 'values ';
 
 print $sql;
 print "\n";
 $results = mysqli_query($conn, $sql);
 $first = 1;
-set_error_handler("catchBadFunction");
+set_error_handler('catchBadFunction');
 while ($player = mysqli_fetch_array($results)) {
-    $funcName = "score".$player["pos"];
-    if ($player["pos"] == "") {
+    $funcName = 'score' . $player['pos'];
+    if ($player['pos'] == '') {
         $pts = 0;
     } else {
         $pts = call_user_func($funcName, $player);
     }
 
     if ($first != 1) {
-        $bigquery .= ", ";
+        $bigquery .= ', ';
     }
     $first = 0;
-    $bigquery .= "(".$player["playerid"].", ".$player["season"]. ", ";
-    $bigquery .= $player["week"].", ".$pts.") ";
+    $bigquery .= '(' . $player['playerid'] . ', ' . $player['season'] . ', ';
+    $bigquery .= $player['week'] . ', ' . $pts . ') ';
 
 }
 restore_error_handler();
@@ -65,20 +66,20 @@ $querySQL = <<<EOD
 EOD;
 
 $sql = "UPDATE playerscores SET active=pts WHERE season=$currentSeason AND week=$week ";
-$sql .= "AND playerid in (";
+$sql .= 'AND playerid in (';
 
 $results = mysqli_query($conn, $querySQL);
 $first = 1;
 while (list($playerid) = mysqli_fetch_row($results)) {
     if ($first != 1) {
-        $sql .= ", ";
+        $sql .= ', ';
     }
     $first = 0;
     $sql .= $playerid;
 }
-$sql .= ")";
+$sql .= ')';
 mysqli_query($conn, $sql);
 
 print "Successfully Transfered scores\n";
 
-?>
+
--- scripts/logscores/transferscores.php
+++ scripts/logscores/transferscores.php
@@ -1,50 +1,51 @@
-<?
-require_once dirname(__FILE__)."/../base.php";
+<?php
+require_once dirname(__FILE__) . '/../base.php';
 
-function catchBadFunction($errno, $errstr, $errfile, $errline, $vars) {
+function catchBadFunction($errno, $errstr, $errfile, $errline, &$vars)
+{
     error_log("$errstr in $errfile on line $errline");
     if ($errno == FATAL || $errno == ERROR) {
         exit(1);
     }
 //    print_r ($vars);
-    print "Problem with player: ".$vars["player"]["playerid"];
-    print " - Pos: ".$vars["player"]["position"]."\n";
-    $vars["pts"] = 0;
+    print 'Problem with player: ' . $vars['player']['playerid'];
+    print ' - Pos: ' . $vars['player']['position'] . "\n";
+    $vars['pts'] = 0;
 }
 
-include "base/scoring.php";
+include 'base/scoring.php';
 
 //$week = $currentWeek - 1;
 $week = $currentWeek;
 
-$sql = "select p.playerid, p.pos, s.season, s.* from newplayers p, stats s ";
+$sql = 'select p.playerid, p.pos, s.season, s.* from newplayers p, stats s ';
 $sql .= "where s.statid=p.flmid and s.played=1 and s.season=$currentSeason and week=$week";
 
-$bigquery = "insert into playerscores (playerid, season, week, pts) ";
-$bigquery .= "values ";
+$bigquery = 'insert into playerscores (playerid, season, week, pts) ';
+$bigquery .= 'values ';
 
-$results = mysqli_query($conn, $sql) or die("Dead on select: " . mysqli_error($conn));
+$results = mysqli_query($conn, $sql) or die('Dead on select: ' . mysqli_error($conn));
 $first = 1;
-set_error_handler("catchBadFunction");
+set_error_handler('catchBadFunction');
 while ($player = mysqli_fetch_array($results)) {
-    $funcName = "score".$player["pos"];
-    if ($player["pos"] == "") {
+    $funcName = 'score' . $player['pos'];
+    if ($player['pos'] == '') {
         $pts = 0;
     } else {
         $pts = call_user_func($funcName, $player);
     }
 
     if ($first != 1) {
-        $bigquery .= ", ";
+        $bigquery .= ', ';
     }
     $first = 0;
-    $bigquery .= "(".$player["playerid"].", ".$player["season"]. ", ";
-    $bigquery .= $player["week"].", ".$pts.") ";
+    $bigquery .= '(' . $player['playerid'] . ', ' . $player['season'] . ', ';
+    $bigquery .= $player['week'] . ', ' . $pts . ') ';
 
 }
 restore_error_handler();
 print $bigquery;
-mysqli_query($conn, $bigquery) or die("Error: " . mysqli_error($conn));
+mysqli_query($conn, $bigquery) or die('Error: ' . mysqli_error($conn));
 
 
 $querySql = <<<EOD
@@ -55,8 +56,8 @@ $querySql = <<<EOD
     WHERE ps.season=$currentSeason AND ps.week=$week
 EOD;
 
-$results = mysqli_query($conn, $querySql) or die("Error: " . mysqli_error($conn));
+$results = mysqli_query($conn, $querySql) or die('Error: ' . mysqli_error($conn));
 
-print "Successfully Transfered " . mysqli_affected_rows($conn) . " scores\n";
+print 'Successfully Transfered ' . mysqli_affected_rows($conn) . " scores\n";
 
 ?>
