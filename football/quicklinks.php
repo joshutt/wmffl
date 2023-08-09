20d716384284a52819c51e8c2ca8e580bb3752e8
Josh <3430+joshutt@users.noreply.github.com>
2023-08-09 16:08:49 -0400

    Update draft date voting

--- football/quicklinks.php
+++ football/quicklinks.php
@@ -1,6 +1,7 @@
 <div class="cat">Other Links</div>
 
 <ul class="list-group">
+    <li class="list-group-item float-left col-lg-4 col-md-6 col-12"><a href="/history/2023Season/draftdate">Draft Date Votes</a></li>
     <li class="list-group-item float-left col-lg-4 col-md-6 col-12"><a href="/history/2022Season/teammoney">Winnings</a></li>
     <li class="list-group-item float-left col-lg-4 col-md-6 col-12"><a href="/history/2023Season/draftorder">2023 Draft Order</a></li>
     <li class="list-group-item float-left col-lg-4 col-md-6 col-12"><a href="/history/2023Season/protectioncost">2023 Protection Costs</a></li>
