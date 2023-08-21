e6f06f2eba95a9d2e7f1f0b4bf2b56cc6c12c57b
Josh <3430+joshutt@users.noreply.github.com>
2023-08-20 21:05:53 -0400

    Added 2023 proposals

--- football/quicklinks.php
+++ football/quicklinks.php
@@ -1,6 +1,7 @@
 <div class="cat">Other Links</div>
 
 <ul class="list-group">
+    <li class="list-group-item float-left col-lg-4 col-md-6 col-12"><a href="/rules/proposals2023">Rule Proposals</a></li>
     <li class="list-group-item float-left col-lg-4 col-md-6 col-12"><a href="/transactions/protections">Protections</a></li>
     <li class="list-group-item float-left col-lg-4 col-md-6 col-12"><a href="/history/2023Season/teammoney">Winnings</a></li>
     <li class="list-group-item float-left col-lg-4 col-md-6 col-12"><a href="/history/2023Season/draftorder">2023 Draft Order</a></li>
---
+++ football/rules/proposals2023.php
@@ -0,0 +1,52 @@
+<?php
+$title = 'WMFFL Rule Proposals';
+
+$cssList = array('rules.css');
+include 'base/menu.php';
+?>
+
+<h1 align=center>Current Rule Proposals</h1>
+<hr SIZE="1"/>
+
+<p>This is the list of proposals to be voted on for the 2023 WMFFL season.
+    If you would like to suggest a rule proposal, you may
+    do so on the <a href="proposesubmit.php">proposals page</a>. The part of
+    each proposal that appears in <i>Italics</i> is what effect this proposal will
+    have on the ruleset, if passed.</p>
+
+<div class="card m-3 p-2 bg-light">
+    <b>Proposal 2023.1 - Admit Andrew Kadish as new owner</b>
+    <b>Sponsor: Tom Marsh</b><br/>
+    <span class="ballot">Status: <span class="status">On Ballot</span></span><br/>
+    Andrew Kadish take over a owner of either the Fighting Squirrels or the Sean Taylor's Ashes.  His choice.
+</div>
+
+<div class="card m-3 p-2 bg-light">
+    <b>Proposal 2023.2 - Unprotected Players Reset Protection Modifier</b>
+    <b>Sponsor: Tom Marsh</b><br/>
+    <span class="ballot">Status: <span class="status">On Ballot</span></span><br/>
+    Players that are unprotected in a given season will have thier protection modifer reset to 0, rather than decline
+    by 2.
+    <blockquote class="mb-0 mt-2 px-3"><i>Change rule X.E.3 to read:<br/>
+            3. If a player is left unprotected his modifier will be reduced to 0.
+        </i></blockquote>
+</div>
+
+
+<div class="card m-3 p-2 bg-light">
+    <b>Proposal 2023.3 - Realign the Divisions</b>
+    <b>Sponsor: Tom Marsh</b><br/>
+    <span class="ballot">Status: <span class="status">On Ballot</span></span><br/>
+    Realign the divisions.  Discuss how realignment should look.
+</div>
+
+<div class="card m-3 p-2 bg-light">
+    <b>Proposal 2023.4 - Activations Due 5 minutes before kickoff</b>
+    <b>Sponsor: Tom Marsh</b><br/>
+    <span class="ballot">Status: <span class="status">On Ballot</span></span><br/>
+    Move the deadline for activations being due to 5 minutes before kickoff, from current rule of 30 minutes
+    <blockquote class="mb-0 mt-2 px-3"><i>Change the '30' in rule V.A.1 to '5'</i></blockquote>
+</div>
+
+
+<?php include 'base/footer.php'; ?>
