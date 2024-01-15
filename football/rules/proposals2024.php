74f6328f00b894daa5b06bd5eb21b525d4ee41c7
Josh <3430+joshutt@users.noreply.github.com>
2024-01-15 17:11:45 -0500

    MAIN-12: Add 2024 Rule Change proposals.  Also draft order

---
+++ football/rules/proposals2024.php
@@ -0,0 +1,59 @@
+<?php
+$title = 'WMFFL Rule Proposals';
+
+$cssList = array('rules.css');
+include 'base/menu.php';
+?>
+
+<h1 align=center>Current Rule Proposals</h1>
+<hr SIZE="1"/>
+
+<div class="container">
+<p>This is the list of proposals to be voted on for the 2024 WMFFL season.
+    If you would like to suggest a rule proposal, you may
+    do so on the <a href="proposesubmit.php">proposals page</a>. The part of
+    each proposal that appears in <i>Italics</i> is what effect this proposal will
+    have on the ruleset, if passed.</p>
+
+<p>To vote on any issues currently open, you may do so on the <a href="ballot">Ballot</a></p>
+
+<div class="card m-3 p-2 bg-light">
+    <b>Proposal 2024.1 - Get rid of IR and increase roster to 26</b>
+    <b>Sponsor: Richard Lawson</b><br/>
+    <span class="ballot">Status: <span class="status">Under Discussion</span></span><br/>
+    I like IR as a concept but I just feel it is too inconsistent to manage.<br/>
+    Couple of occasions where players have been activated Sunday morning on the site and resulted in dropping players from squads. Jason Myers for Bulldogs and De'Von Achane for Lionhearts. That is fine but I feel it is inconsistent.<br/>
+    Irregardless I think it's too tricky to navigate for us and I think I would shelve it and go 26 man leagues....up to you if you hold on to players who are injured on not.
+    <blockquote class="mb-0 mt-2 px-3"><i>Change text in rule IV.A from '25' to '26'<br/>
+            Repeal rule IV.C
+        </i></blockquote>
+</div>
+
+<div class="card m-3 p-2 bg-light">
+    <b>Proposal 2024.2 - Dealing with players coming off IR late in week</b>
+    <b>Sponsor: Josh Utterback</b><br/>
+    <span class="ballot">Status: <span class="status">Under Discussion</span></span><br/>
+    There has been a lot of inconsiency with when players come off IR causing teams to be unexpectantly over roster limits.  To prevent this
+    any player coming off of IR on Saturday or Sunday (due to removal from NFL IR) will not count against the roster limit for that week, unless that player was activated
+    by the WMFFL team.  (Also removes obsolete COVID list rule)
+    <blockquote class="mb-0 mt-2 px-3"><i>Change rule IV.C.4 to read:<br/>
+            <blockquote>
+            4. Any player on the IR on Friday and removed from the NFL IR on Saturday or Sunday of a given week, will not count against the
+            roster limits in IV.A for that given week unless they are activated as a starter by the WMFFL team.</blockquote>
+        </i></blockquote>
+</div>
+
+
+<div class="card m-3 p-2 bg-light">
+    <b>Proposal 2024.3 - Increase Penalties for Bad Activations</b>
+    <b>Sponsor: Josh Utterback</b><br/>
+    <span class="ballot">Status: <span class="status">Under Discussion</span></span><br/>
+    Increase the bye week penalty to 5 points<br/>
+    Increase all other activations violations to $5 fines and 10 point penalties
+    <blockquote class="mb-0 mt-2 px-3"><i>Change text in rule V.A.3.a from "2 point penalty" to "5 point penalty"<br/>
+            Change text in rules V.A.3.(b-d) from "a $1 fine and a 2 point penalty" to "a $5 fine and a 10 point penalty"
+        </i></blockquote>
+</div>
+
+</div>
+<?php include 'base/footer.php'; ?>
