0fa839a8dcf09a3e7d2475e2703e91b55f0e60e7
Josh <3430+joshutt@users.noreply.github.com>
2024-01-15 13:29:25 -0500

    MAIN-11: Updated team money to account for 2023 season

--- football/history/2023Season/teammoney.php
+++ football/history/2023Season/teammoney.php
@@ -9,7 +9,7 @@ include 'base/menu.php';
 
 
 <H1 ALIGN=Center>Team Finances</H1>
-<H5 ALIGN=Center>Last Updated 11/26/2023</H5>
+<H5 ALIGN=Center>Last Updated 1/15/2024</H5>
 <HR size="1">
 
 <p>
@@ -20,7 +20,7 @@ include 'base/menu.php';
 <div class="center">
 
     <?php
-    $amt_owed = array( );
+    $amt_owed = array( 6 => 68.40, 9=>63.20, 12=>5.75, 5=>63.35);
 
     if ($isin && array_key_exists($teamnum, $amt_owed)) { ?>
 
@@ -38,7 +38,7 @@ include 'base/menu.php';
             <th>Wins</th>
             <th>Playoffs</th>
             <th>Balance</th>
-            <th class="d-none">2023 Fee</th>
+            <th>2024 Fee</th>
         <tr>
         <tr class="oddRow">
             <td class="name padded">Aint Nothing But a Jew Thing</td>
@@ -47,10 +47,10 @@ include 'base/menu.php';
             <td class="padded">-</td>
             <td class="padded">11</td>
             <td class="padded">-</td>
-            <td class="padded">3 x $2.89</td>
+            <td class="padded">5 x $2.95</td>
             <td class="padded">-</td>
-            <td class="padded"><span class="debt">($2.33)</span></td>
-            <td class="padded d-none">$0.00</td>
+            <td class="padded">$3.75</td>
+            <td class="padded">$0.00</td>
         </tr>
         <tr class="evenRow">
             <td class="name padded">Amish Electricians</td>
@@ -59,10 +59,10 @@ include 'base/menu.php';
             <td class="padded">-</td>
             <td class="padded">-</td>
             <td class="padded">13</td>
-            <td class="padded">1 x $2.89</td>
+            <td class="padded">3 x $2.95</td>
             <td class="padded">-</td>
-            <td class="padded">$85.16</td>
-            <td class="padded d-none">$0.00</td>
+            <td class="padded">$91.12</td>
+            <td class="padded">$0.00</td>
         </tr>
         <tr class="oddRow">
             <td class="name padded">British Bulldogs</td>
@@ -71,22 +71,22 @@ include 'base/menu.php';
             <td class="padded">-</td>
             <td class="padded">1</td>
             <td class="padded">-</td>
-            <td class="padded">11 x $2.89</td>
-            <td class="padded">Playoff Team - $24.30</td>
-            <td class="padded">$55.09</td>
-            <td class="padded d-none">$0.00</td>
+            <td class="padded">12 x $2.95</td>
+            <td class="padded">Division Title - $24.80<br/>Playoff Team - $24.80<br/>First Round Win - $99.20</td>
+            <td class="padded">$182.57</td>
+            <td class="padded">$0.00</td>
         </tr>
         <tr class="evenRow">
             <td class="name padded">Crusaders</td>
             <td class="padded"><span class="debt">($92.44)</span></td>
             <td class="padded">$167.44</td>
             <td class="padded">-</td>
+            <td class="padded">10</td>
             <td class="padded">7</td>
-            <td class="padded">7</td>
-            <td class="padded">5 x $2.89</td>
+            <td class="padded">8 x $2.95</td>
             <td class="padded">-</td>
-            <td class="padded">$0.45</td>
-            <td class="padded d-none">$0.00</td>
+            <td class="padded">$6.60</td>
+            <td class="padded">$68.40</td>
         </tr>
         <tr class="oddRow">
             <td class="name padded">Gallic Warriors</td>
@@ -95,10 +95,10 @@ include 'base/menu.php';
             <td class="padded">-</td>
             <td class="padded">-</td>
             <td class="padded">-</td>
-            <td class="padded">3 x $2.89</td>
+            <td class="padded">4 x $2.95</td>
             <td class="padded">-</td>
-            <td class="padded">$8.67</td>
-            <td class="padded d-none">$0.00</td>
+            <td class="padded">$11.80</td>
+            <td class="padded">$63.20</td>
         </tr>
         <tr class="evenRow">
             <td class="name padded">MeggaMen</td>
@@ -106,11 +106,11 @@ include 'base/menu.php';
             <td class="padded">-</td>
             <td class="padded">-</td>
             <td class="padded">-</td>
-            <td class="padded">11</td>
-            <td class="padded">9 x $2.89</td>
-            <td class="padded">-</td>
-            <td class="padded">$284.65</td>
-            <td class="padded d-none">$0.00</td>
+            <td class="padded">21</td>
+            <td class="padded">10 x $2.95</td>
+            <td class="padded">Playoff Team - $24.80<br/>First Round Win - $99.20<br/>Championship - $124.00</td>
+            <td class="padded">$526.14</td>
+            <td class="padded">$0.00</td>
         </tr>
         <tr class="oddRow">
             <td class="name padded">Norsemen</td>
@@ -118,11 +118,11 @@ include 'base/menu.php';
             <td class="padded">-</td>
             <td class="padded">-</td>
             <td class="padded">4</td>
-            <td class="padded">3</td>
-            <td class="padded">6 x $2.89</td>
+            <td class="padded">6</td>
+            <td class="padded">7 x $2.95</td>
             <td class="padded">-</td>
-            <td class="padded">$468.69</td>
-            <td class="padded d-none">$0.00</td>
+            <td class="padded">$469.00</td>
+            <td class="padded">$0.00</td>
         </tr>
         <tr class="evenRow">
             <td class="name padded">Richard's Lionhearts</td>
@@ -130,11 +130,11 @@ include 'base/menu.php';
             <td class="padded">$82.56</td>
             <td class="padded">-</td>
             <td class="padded">-</td>
-            <td class="padded">-</td>
-            <td class="padded">5 x $2.89</td>
-            <td class="padded">-</td>
-            <td class="padded">$14.45</td>
-            <td class="padded d-none">$0.00</td>
+            <td class="padded">1</td>
+            <td class="padded">7 x $2.95</td>
+            <td class="padded">Division Title - $24.80<br/>Playoff Team - $24.80</td>
+            <td class="padded">$69.25</td>
+            <td class="padded">$5.75</td>
         </tr>
         <tr class="oddRow">
             <td class="name padded">Sacks On the Beach</td>
@@ -142,11 +142,11 @@ include 'base/menu.php';
             <td class="padded">$46.50</td>
             <td class="padded">-</td>
             <td class="padded">-</td>
-            <td class="padded">7</td>
-            <td class="padded">5 x $2.89</td>
+            <td class="padded">9</td>
+            <td class="padded">7 x $2.95</td>
             <td class="padded">-</td>
-            <td class="padded">$7.45</td>
-            <td class="padded d-none">$0.00</td>
+            <td class="padded">$11.65</td>
+            <td class="padded">$63.35</td>
         </tr>
         <tr class="evenRow">
             <td class="name padded">Sigourney's Weaver's</td>
@@ -155,10 +155,10 @@ include 'base/menu.php';
             <td class="padded">-</td>
             <td class="padded">6</td>
             <td class="padded">-</td>
-            <td class="padded">3 x $2.89</td>
+            <td class="padded">3 x $2.95</td>
             <td class="padded">-</td>
-            <td class="padded">$2.67</td>
-            <td class="padded d-none">$0.00</td>
+            <td class="padded">$2.85</td>
+            <td class="padded">$0.00</td>
         </tr>
         <tr class="oddRow">
             <td class="name padded">Testudos Revenge</td>
@@ -167,22 +167,22 @@ include 'base/menu.php';
             <td class="padded">-</td>
             <td class="padded">2</td>
             <td class="padded">-</td>
-            <td class="padded">7 x $2.89</td>
+            <td class="padded">8 x $2.95</td>
             <td class="padded">-</td>
-            <td class="padded">$262.01</td>
-            <td class="padded d-none">$0.00</td>
+            <td class="padded">$265.38</td>
+            <td class="padded">$0.00</td>
         </tr>
         <tr class="evenRow">
             <td class="name padded">Trump Molests Collies</td>
             <td class="padded">$412.65</td>
             <td class="padded">-</td>
             <td class="padded">-</td>
+            <td class="padded">1</td>
             <td class="padded">-</td>
-            <td class="padded">-</td>
-            <td class="padded">8 x $2.89</td>
-            <td class="padded">-</td>
-            <td class="padded">$360.77</td>
-            <td class="padded d-none">$0.00</td>
+            <td class="padded">10 x $2.95</td>
+            <td class="padded">Division Title - $24.80<br/>Playoff Team - $24.80</td>
+            <td class="padded">$415.75</td>
+            <td class="padded">$0.00</td>
         </tr>
     </table>
     <p>Previous column is based on <a href="/history/2022Season/teammoney">2022  results</a></p>
