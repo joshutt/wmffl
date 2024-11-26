30b1c51f2b5760a36b01d7198645ab1c22ace128
josh.utterback@gmail.com <3430+joshutt@users.noreply.github.com>
2024-11-26 10:45:15 -0500

    Updates for Week 11

--- football/history/2024Season/standings.php
+++ football/history/2024Season/standings.php
@@ -9,7 +9,7 @@ if ($thisWeek == '') {
 $thisSeason = 2024;
 $title = 'Standings';
 
-$clinchedList = array( "Sigourney's Beaver" => 'e-', 'Amish Electricians' => 'e-', 'British Bulldogs' => 'e-', 'MeggaMen' => 'e-' );
+$clinchedList = array();
 
 include 'base/menu.php';
 ?>
--- football/history/2024Season/teammoney.php
+++ football/history/2024Season/teammoney.php
@@ -13,7 +13,7 @@ include 'base/menu.php';
 
 
 <H1 ALIGN=Center>Team Finances</H1>
-<H5 ALIGN=Center>Last Updated 11/16/2024</H5>
+<H5 ALIGN=Center>Last Updated 9/28/2024</H5>
 
 <p>
     <?php include 'base/statbar.html' ?>
@@ -23,181 +23,168 @@ include 'base/menu.php';
 <div class="center">
 
     <?php
-    $amt_owed = array( 6 => 67.00 );
+    $amt_owed = array( 6 => 82.67 );
 
     if ($isin && array_key_exists($teamnum, $amt_owed)) { ?>
 
         <h2 align="center"><a class="btn btn-wmffl" href="https://paypal.me/JoshUtterback/<?= $amt_owed[$teamnum] ?>">Pay Now</a></h2>
     <?php } ?>
 
-    <table class="report table table-striped table-hover">
+    <table class="report">
         <tr class="titleRow">
             <th>Team</th>
             <th>Previous</th>
             <th>Paid</th>
             <th>Late Fees</th>
             <th>Illegal<br/>Lineup</th>
-            <th>Bye Week<br/>Activation</th>
             <th>Extra<br/>Transactions</th>
             <th>Wins</th>
             <th>Playoffs</th>
             <th>Balance</th>
             <th>2024 Fee</th>
         <tr>
-        <tr class="">
+        <tr class="oddRow">
             <td class="name padded">Aint Nothing But a Jew Thing</td>
             <td class="padded">$3.75</td>
             <td class="padded">-</td>
             <td class="padded">-</td>
             <td class="padded">-</td>
             <td class="padded">-</td>
+            <td class="padded">1 x $2.73</td>
             <td class="padded">-</td>
-            <td class="padded">6 x $2.90</td>
-            <td class="padded">-</td>
-            <td class="padded">$21.15</td>
+            <td class="padded">$6.48</td>
             <td class="padded">$0.00</td>
         </tr>
-        <tr class="">
+        <tr class="evenRow">
             <td class="name padded">Amish Electricians</td>
             <td class="padded">$170.27</td>
             <td class="padded">-</td>
             <td class="padded">-</td>
             <td class="padded">-</td>
             <td class="padded">-</td>
+            <td class="padded">1 x $2.73</td>
             <td class="padded">-</td>
-            <td class="padded">3 x $2.90</td>
-            <td class="padded">-</td>
-            <td class="padded">$103.97</td>
+            <td class="padded">$98.00</td>
             <td class="padded">$0.00</td>
         </tr>
-        <tr class="">
+        <tr class="oddRow">
             <td class="name padded">British Bulldogs</td>
             <td class="padded">$182.57</td>
             <td class="padded">-</td>
             <td class="padded">-</td>
             <td class="padded">-</td>
             <td class="padded">-</td>
+            <td class="padded">1 x $2.73</td>
             <td class="padded">-</td>
-            <td class="padded">4 x $2.90</td>
-            <td class="padded">-</td>
-            <td class="padded">$119.17</td>
+            <td class="padded">$110.30</td>
             <td class="padded">$0.00</td>
         </tr>
-        <tr class="table-danger">
+        <tr class="evenRow">
             <td class="name padded">Crusaders</td>
             <td class="padded">$6.60</td>
             <td class="padded">-</td>
-            <td class="padded">$16.00</td>
+            <td class="padded">$11.00</td>
             <td class="padded">-</td>
             <td class="padded">-</td>
+            <td class="padded">1 x $2.73</td>
             <td class="padded">-</td>
-            <td class="padded">6 x $2.90</td>
-            <td class="padded">-</td>
-            <td class="padded"><span class="debt">($67.00)</span></td>
-            <td class="padded">$67.00</td>
+            <td class="padded"><span class="debt">($82.67)</span></td>
+            <td class="padded">$82.67</td>
         </tr>
-        <tr class="">
+        <tr class="oddRow">
             <td class="name padded">Gallic Warriors</td>
             <td class="padded">$11.80</td>
             <td class="padded">$63.20</td>
             <td class="padded">-</td>
             <td class="padded">-</td>
             <td class="padded">-</td>
-            <td class="padded">10</td>
-            <td class="padded">5 x $2.90</td>
+            <td class="padded">1 x $2.73</td>
             <td class="padded">-</td>
-            <td class="padded">$4.50</td>
+            <td class="padded">$2.73</td>
             <td class="padded">$0.00</td>
         </tr>
-        <tr class="">
+        <tr class="evenRow">
             <td class="name padded">MeggaMen</td>
             <td class="padded">$526.14</td>
             <td class="padded">-</td>
             <td class="padded">-</td>
             <td class="padded">-</td>
             <td class="padded">-</td>
-            <td class="padded">21</td>
-            <td class="padded">4 x $2.90</td>
+            <td class="padded">0 x $2.73</td>
             <td class="padded">-</td>
-            <td class="padded">$441.74</td>
+            <td class="padded">$451.14</td>
             <td class="padded">$0.00</td>
         </tr>
-        <tr class="">
+        <tr class="oddRow">
             <td class="name padded">Norsemen</td>
             <td class="padded">$469.00</td>
             <td class="padded">-</td>
             <td class="padded">-</td>
             <td class="padded">-</td>
             <td class="padded">-</td>
-            <td class="padded">7</td>
-            <td class="padded">5 x $2.90</td>
+            <td class="padded">2 x $2.73</td>
             <td class="padded">-</td>
-            <td class="padded">$401.50</td>
+            <td class="padded">$399.46</td>
             <td class="padded">$0.00</td>
         </tr>
-        <tr class="">
+        <tr class="evenRow">
             <td class="name padded">Richard's Lionhearts</td>
             <td class="padded">$69.27</td>
             <td class="padded">$11.75</td>
             <td class="padded">$6.00</td>
             <td class="padded">-</td>
             <td class="padded">-</td>
+            <td class="padded">3 x $2.73</td>
             <td class="padded">-</td>
-            <td class="padded">7 x $2.90</td>
-            <td class="padded">-</td>
-            <td class="padded">$20.30</td>
+            <td class="padded">$8.19</td>
             <td class="padded">$0.00</td>
         </tr>
-        <tr class="">
+        <tr class="oddRow">
             <td class="name padded">Sacks On the Beach</td>
             <td class="padded">$11.65</td>
             <td class="padded">$63.35</td>
             <td class="padded">-</td>
             <td class="padded">-</td>
             <td class="padded">-</td>
-            <td class="padded">11</td>
-            <td class="padded">5 x $2.90</td>
+            <td class="padded">2 x $2.73</td>
             <td class="padded">-</td>
-            <td class="padded">$3.50</td>
+            <td class="padded">$5.46</td>
             <td class="padded">$0.00</td>
         </tr>
-        <tr class="">
+        <tr class="evenRow">
             <td class="name padded">Sigourney's Beaver</td>
             <td class="padded">$2.85</td>
             <td class="padded">-</td>
             <td class="padded">-</td>
             <td class="padded">-</td>
-            <td class="padded">4</td>
             <td class="padded">-</td>
-            <td class="padded">2 x $2.90</td>
+            <td class="padded">2 x $2.73</td>
             <td class="padded">-</td>
-            <td class="padded">$4.65</td>
+            <td class="padded">$8.31</td>
             <td class="padded">$0.00</td>
         </tr>
-        <tr class="">
+        <tr class="oddRow">
             <td class="name padded">Testudos Revenge</td>
             <td class="padded">$265.38</td>
             <td class="padded">-</td>
             <td class="padded">-</td>
             <td class="padded">-</td>
             <td class="padded">-</td>
+            <td class="padded">1 x $2.73</td>
             <td class="padded">-</td>
-            <td class="padded">7 x $2.90</td>
-            <td class="padded">-</td>
-            <td class="padded">$210.68</td>
+            <td class="padded">$193.11</td>
             <td class="padded">$0.00</td>
         </tr>
-        <tr class="">
+        <tr class="evenRow">
             <td class="name padded">Trump Molests Collies</td>
             <td class="padded">$415.75</td>
             <td class="padded">-</td>
             <td class="padded">-</td>
             <td class="padded">-</td>
             <td class="padded">-</td>
+            <td class="padded">3 x $2.73</td>
             <td class="padded">-</td>
-            <td class="padded">6 x $2.90</td>
-            <td class="padded">-</td>
-            <td class="padded">$358.15</td>
+            <td class="padded">$348.94</td>
             <td class="padded">$0.00</td>
         </tr>
     </table>
