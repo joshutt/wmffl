04ff23250c4d7ac15346cb23168bcf5aecea4ade
Josh <3430+joshutt@users.noreply.github.com>
2025-06-29 16:36:51 -0400

    Remove live draft files.  These have been moved to their own project

--- football/transactions/livedraft/getLastChat.php
+++
@@ -1,9 +0,0 @@
-<?
-require_once "utils/start.php";
-
-$sql = "SELECT MAX(messageId) FROM chat ";
-$results = mysqli_query($conn, $sql) or die("Die: " + mysqli_error($conn));
-list($message) = mysqli_fetch_row($results);
-
-print $message;
-?>
