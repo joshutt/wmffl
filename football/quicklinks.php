f18c10a17befba8e5c011de1a30059f49db16e20
Josh <3430+joshutt@users.noreply.github.com>
2025-09-13 17:52:42 -0400

    Sync up quicklinks

--- football/quicklinks.php
+++ football/quicklinks.php
@@ -1,7 +1,7 @@
 <div class="cat">Other Links</div>
 
 <ul class="list-group">
-    <li class="list-group-item float-left col-lg-4 col-md-6 col-12"><a href="/history/2025Season/draftresults">Draft Resutls</a></li>
+    <li class="list-group-item float-left col-lg-4 col-md-6 col-12"><a href="/history/2025Season/draftresults">Draft Results</a></li>
     <li class="list-group-item float-left col-lg-4 col-md-6 col-12"><a href="/history/teammoney">Finances</a></li>
 </ul>
 
