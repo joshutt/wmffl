7182109fdc2cf7d9440a478b02af053e834a5b94
Josh <joshutt@users.noreply.github.com>
2026-07-25 21:19:35 -0400

    Add 2026 WMFFL rule proposals page with draft reductions, IR limits, kicker scoring updates, and blocked kicks discussion

---
+++ football/rules/proposals2026.php
@@ -0,0 +1,78 @@
+<?php
+$title = 'WMFFL Rule Proposals';
+
+$cssList = array('/base/css/rules.css');
+include 'base/menu.php';
+?>
+
+<h1 align=center>Current Rule Proposals</h1>
+<hr SIZE="1"/>
+
+<div class="container">
+    <p>This is the list of proposals to be voted on for the 2026 WMFFL season.</p>
+
+    <p>To vote on any issues currently open, you may do so on the <a href="ballot">Ballot</a></p>
+
+    <div class="card m-3 p-2 bg-light">
+        <b>Proposal 2026.1 - Reduce Roster Size</b>
+        <b>Sponsor: Richard Lawson</b><br/>
+        <span class="ballot">Status: <span class="status">In Discussion</span></span><br/>
+        <p>The draft to go from 16 rounds to 14 rounds. The active squad size to be 23 players + head coach (from 25 +
+            head coach).  The maximum squad size to be 24 players + head coach (from 26 + head coach) including IR
+            players
+        </p>
+
+        <blockquote class="mb-0 mt-2 px-3"><i>Change text in rule VI.D to be "The draft will be 14 rounds"<br/>
+                Change the text in rule IV.A from "25" to "23"<br/>
+                Change the text in rule IV.C.5 from "26" to "24"
+            </i></blockquote>
+    </div>
+
+    <div class="card m-3 p-2 bg-light">
+        <b>Proposal 2026.2 - No Limit to Number of IR players</b>
+        <b>Sponsor: Byron Williams</b><br/>
+        <span class="ballot">Status: <span class="status">In Discussion</span></span><br/>
+        <p>Remove the limit of only 1 IR player.  If a player is hurt and on the NFL IR then we should be allowed to put
+            them on the IR.  Whether it is the first player on our team or the tenth.</p>
+        <blockquote class="mb-0 mt-2 px-3"><i>Repeal rule IV.C.5
+            </i></blockquote>
+    </div>
+
+
+    <div class="card m-3 p-2 bg-light">
+        <b>Proposal 2026.3 - Blocked Kicks</b>
+        <b>Sponsor: Josh Utterback</b><br/>
+        <span class="ballot">Status: <span class="status">In Discussion</span></span><br/>
+        <p>In 2006 we passed a rule giving players 3 points for every blocked kick.  It was added to the scoring rules,
+            but never added to the actual rule book.  Richard found this during a game last season.  It has been
+            happening less than 1 game a year.</p>
+
+         <p>Should we formally add the blocked kicks or adjust the scoring to match the rule book </p>
+        <blockquote class="mb-0 mt-2 px-3"><i>Add rules V.B.3.e, V.B.4.e and V.B.7.f all reading:
+                <blockquote><i>Blocked Kick<br/>
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;i. Each one = 3 pts</i></blockquote>
+            </i></blockquote>
+    </div>
+
+
+    <div class="card m-3 p-2 bg-light">
+        <b>Proposal 2026.4 - Reduce Kicker Points</b>
+        <b>Sponsor: Tom Marsh</b><br/>
+        <span class="ballot">Status: <span class="status">In Discussion</span></span><br/>
+        <p>Reduce kicker points.<br/>
+            Field Goal:<br/>
+            Made 0-39 yards = 2 pts<br/>
+            Made 40-49 yards = 2 pts<br/>
+            Made 50-59 yards = 4 pts<br/>
+            Made 60+ yards = 6 pts<br/>
+        </p>
+
+        <blockquote class="mb-0 mt-2 px-3"><i>Change text in rule V.B.5.b.i from "3 pts" to "2 pts"<br/>
+                Change text in rule V.B.5.b.ii from "4 pts" to "2 pts"<br/>
+                Change text in rule V.B.5.b.iii from "5 pts" to "4 pts"<br/>
+                Change text in rule V.B.5.b.iv from "7 pts" to "6 pts"<br/>
+            </i></blockquote>
+    </div>
+
+</div>
+<?php include 'base/footer.php'; ?>
